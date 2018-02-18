4e734e8c037ee7f05cf7b4afcfe3c0b4f17f4718
wardbradt <bbradt@andover.edu>
2018-02-18 16:03:52 -0500

    Revert "some changes"
    This reverts commit dbf015bcb49eba780c29617f469e5a582c3663b4.

--- pyanalytics/dashboard/urls.py
+++ pyanalytics/dashboard/urls.py
@@ -1,10 +1,9 @@
-from django.urls import include, path
+from django.urls import path
 from . import views
 
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('accounts/', include('django.contrib.auth.urls')),
     path('reports/', views.ReportListView.as_view(), name='reports'),
     path('report/<int:pk>', views.ReportDetailView.as_view(), name='report-detail'),
 ]
