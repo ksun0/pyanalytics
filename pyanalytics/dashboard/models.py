359244950a6faa4d5f0fc1acc765961f98829455
wardbradt <bbradt@andover.edu>
2018-02-18 16:04:27 -0500

    Revert "Revert "some changes""
    This reverts commit 4e734e8c037ee7f05cf7b4afcfe3c0b4f17f4718.

--- pyanalytics/dashboard/models.py
+++ pyanalytics/dashboard/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.urls import reverse  # Used to generate URLs by reversing the URL patterns
 
 
-# Create your models here.
 class Report(models.Model):
     """
     Model representing an Analytics Report
--- pyanalytics/dashboard/urls.py
+++ pyanalytics/dashboard/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
 
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('reports/', views.ReportListView.as_view(), name='reports'),
     path('report/<int:pk>', views.ReportDetailView.as_view(), name='report-detail'),
 ]
